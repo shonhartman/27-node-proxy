78aeb53a387e0d7cbedfd2fbfade2b9c065cfa7b
Shaun Hartman <shaunhartman@Shauns-MacBook-Pro.local>
2016-04-13 13:29:36 -0500

    ...

--- app.js
+++ app.js
@@ -11,105 +11,44 @@ let server = http.createServer(function(req, res) {
    *
    * 1. parsedUrl.path contains the path the user is requesting.
    *    Setup an if statement to check if they're looking at /
-   *
+   */
+
+
+   if (req.url ==="/") {
+
+
+
+   /**
    *    If they are, use req.write() to send them a message
    *    explaining that they're on the root page.
-   *
+   */
+
+   res.write("<h1>You're on the root page</h1>")
+ }
+
+
+/**
    * 2. If they're looking at /other then use req.write() to
    *    send a message explaining that they're on the other page.
-   *
+*/
+
+   else {
+     res.write("<h1>You're not on the root page</h1>")
+   }
+
+/**
    * 3. Remember to use req.end() after you're done using req.write()
    *    so that you actually send a response to the browser.
-   *
+*/
+
+   res.end;
+
+
+/**
    * 4. You should be able to run npm run app and visit localhost:8000
    *    in your browser. At /, you should see the / message. At /other,
    *    you should see the other message.
    */
-
-});
-
-server.listen(8000);
-
-//////////////////////////////////////////////////////////////////////////////
-//LECTURE NOTES
-// proxy.js
-
-import http from 'http';
-
-let server = http.createServer((req, res) => {
-
-    if (req.url ==="/") {
-      res.write("<h1>You're on the root page</h1>")
-    }
-    else {
-      res.write("You're not on the root page")
-    }
-
-    // console.log(req);
-  // console.log("Somebody tried to get a file");
-
-  // res.write("<h1>Hello this is dog</h1>");
-  // res.write("<p>Banana Gram</p>");
-  // res.end();
-
-});
-
-server.listen(8000);
-
-//npm run app
-//"somebody tried to get a file"
-
-//////////////////////////////////////////////////////////////////////////////
-//app.js
-
-import fs from 'fs';
-
-let server = http.createServer((req, res) => {
-  if (req.url ==="/") {
-    fs.readFile('./root')...
-  }
-})
-
-//DOCS
-//node-http-proxy via github
-
-//////////////////////////////////////////////////////////////////////////////
-//app.js
-
-let proxy = httpProxy.createProxyServer({});
-
-let server = httpProxy.createServer((req, res) => {
-
-  proxy.web(req, res, {
-    target: "http://www.google.com"
-  });
-
-});
-
-server.listen(8000);
-
-//iterm
-//npm run app
-
-//////////////////////////////////////////////////////////////////////////////
-//app.js
-
-let proxy = httpProxy.createProxyServer({});
-
-let server = httpProxy.createServer((req, res) => {
-
-    //http://www.openweathermap...
-    //APPID=d494...
-    let parsedUrl = url.parse(req.url, true);
-    parsedUrl.query.APPID = 'd494...';
-    console.log(parsedUrl);
-    parsedUrl.search = null;
-    req.url = url.format(parsedUrl);
-
-  proxy.web(req, res, {
-    target: "http://www.openweathermap/../.."
-
-  });
 
 });
 
